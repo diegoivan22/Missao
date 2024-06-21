0a5c6bc5b8f0ad243502fd4856cf794a1f95a356
diegoivan22 <114594983+diegoivan22@users.noreply.github.com>
2024-06-21 12:25:30 -0300

    21/06

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultados = document.querySelector(".caixa-resultados");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas1 = [
+    {
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "alternativa 1",
+        "alternativa 2"
+    ]
+},
+{
+    enunciado: "Pergunta 1",
+    alternativas: [
+        "alternativa 1",
+        "alternativa 2"
+    ]
+}
+
+     ]
